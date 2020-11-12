7f4efd0bacaf54d903df273b960f73ca8ad6b151
Kevin Kunzmann <kevinkunzmann@gmx.net>
2020-11-12 19:44:13 +0000

    ...

--- .binder/install.R
+++ .binder/install.R
@@ -1,2 +1,2 @@
 options(repos = c(CRAN = sprintf("https://mran.revolutionanalytics.com/snapshot/%s", Sys.getenv("MRAN_DATE"))))
-remotes::install_deps(dependencies = TRUE, pkgdir = "..")
+devtools::install_deps(dependencies = TRUE, pkgdir = "..")
